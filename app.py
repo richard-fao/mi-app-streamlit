e832447fdbcda909718e6dc69c70aecae195fdff
richard-fao <72524120+richard-fao@users.noreply.github.com>
2025-11-12 15:31:01 -0500

    Comment out altair import in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import time
 from datetime import date
 import numpy as np
 import pandas as pd
-import altair as alt
+#import altair as alt
 import streamlit as st
 import plotly.express as px
 import base64
